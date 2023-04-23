8db1fe98da216fe0614f881e066703cb148f4785
Vivek Kumar Rahul <vivekrahul91@gmail.com>
2023-04-23 06:28:36 +0530

    first try

---
+++ accounts/views.py
@@ -0,0 +1,30 @@
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from django.contrib.auth.models import User, auth
+
+# Create your views here.
+
+
+def register(request):
+    if request.method == 'POST':
+        first_name = request.POST['first_name']
+        last_name = request.POST['last_name']
+        username = request.POST['username']
+        email = request.POST['email']
+        password1 = request.POST['password1']
+        password2 = request.POST['password2']
+
+        if password1 == password2:
+            if User.objects.filter(username=username).exists():
+                print("username taken")
+            elif User.objects.filter(email=email).exists():
+                print('email taken')
+            else:
+                user = User.objects.create_user(
+                    username=username, password=password1, email=email, first_name=first_name, last_name=last_name)
+                user.save()
+                print('user created')
+                return redirect('/')
+
+    else:
+        return render(request, 'register.html')
